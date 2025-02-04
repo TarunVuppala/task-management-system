b7240e78bb44bc489e4b04133784f2d1f07324ff
sel-tarun <tarun.vuppala@aceec.ac.in>
2025-02-04 11:53:04 +0530

    shadcn init

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <>
-      Hello
+      <h1>Home</h1>
+      <Button>Button</Button>
     </>
   );
 }
